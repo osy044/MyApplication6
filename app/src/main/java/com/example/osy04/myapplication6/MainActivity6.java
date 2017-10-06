a360091b837bd040754e050d58a83b4903306675
OSY <osy044@naver.com>
2017-10-07 02:27:09 +0900

    first commit

---
+++ app/src/main/java/com/example/osy04/myapplication6/MainActivity6.java
@@ -0,0 +1,13 @@
+package com.example.osy04.myapplication6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+    }
+}
